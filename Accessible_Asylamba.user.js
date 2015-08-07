2989312b84bdd8bbaef22a335e6f54582795055c
Genroa <genroa@hotmail.fr>
2015-08-07 09:36:31 +0200

    first commit

---
+++ Accessible_Asylamba.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Accessible Asylamba
+// @namespace   asylamba
+// @description Userscript dédié à rendre Asylamba accessible aux lecteurs d'écran
+// @include     http://game.asylamba.com/*
+// @match       http://game.asylamba.com/*
+// @grant       GM_xmlhttpRequest
+// @updateURL   https://github.com/Genroa/accessibleasylamba/
+// @version     1.0.0
+// @grant       Genroa
+// @author      Genroa
+// @require		http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
+// ==/UserScript==
+
