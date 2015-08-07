98c47dd020e8515bb82e1ac3af37cd804624f978
Genroa <genroa@hotmail.fr>
2015-08-07 09:47:25 +0200

    work on menu

--- Accessible_Asylamba.user.js
+++ Accessible_Asylamba.user.js
@@ -6,9 +6,22 @@
 // @match       http://game.asylamba.com/*
 // @grant       GM_xmlhttpRequest
 // @updateURL   https://github.com/Genroa/Accessible-Asylamba/raw/master/Accessible_Asylamba.user.js
-// @version     1.0.0
+// @version     1.0.1
 // @grant       Genroa
 // @author      Genroa
 // @require		http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
 // ==/UserScript==
 
+
+function modifyMenu()
+{
+	$.each($('.left-2 > a'), function(){
+		$(this).html($(this).attr("title"));
+	});
+}
+
+
+
+$(function(){
+	modifyMenu();
+});
